54d3a20c24d72ef6c7d92e6c7c95f6c0f98588a2
Isaac Asamoah <juniorike69@gmail.com>
2025-06-14 23:02:34 +0000

    Move: src/partials/Header.jsx to src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,7 @@ function Header() {
   });
 
   return (
-    <header className="absolute w-full z-30">
+    <header className="sticky top-0 z-30 bg-gray-900 shadow-lg">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="flex items-center justify-between h-20">
 
@@ -51,10 +51,13 @@ function Header() {
             {/* Desktop sign in links */}
             <ul className="flex grow justify-end flex-wrap items-center">
               <li>
-                <Link to="/signin" className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">Sign in</Link>
+                <Link to="/about" className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">About</Link>
               </li>
               <li>
-                <Link to="/signup" className="btn-sm text-white bg-purple-600 hover:bg-purple-700 ml-3">Sign up</Link>
+                <Link to="/projects" className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">Projects</Link>
+              </li>
+              <li>
+                <Link to="/contact" className="btn-sm text-white bg-purple-600 hover:bg-purple-700 ml-3">Contact</Link>
               </li>
             </ul>
 
@@ -77,10 +80,13 @@ function Header() {
             <nav id="mobile-nav" ref={mobileNav} className="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out" style={mobileNavOpen ? { maxHeight: mobileNav.current.scrollHeight, opacity: 1 } : { maxHeight: 0, opacity: .8 } }>
               <ul className="bg-gray-800 px-4 py-2">
                 <li>
-                  <Link to="/signin" className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center">Sign in</Link>
+                  <Link to="/about" className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center">About</Link>
                 </li>
                 <li>
-                  <Link to="/signup" className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out">Sign up</Link>
+                  <Link to="/projects" className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center">Projects</Link>
+                </li>
+                <li>
+                  <Link to="/contact" className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out">Contact</Link>
                 </li>
               </ul>
             </nav>
